4fbd0c7a22a21dccc420ed3871463c2ec1c50eb5
EC2 Default User <ec2-user@ip-172-31-0-13.us-east-2.compute.internal>
2021-10-10 04:58:39 +0000

    jobing

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,15 +6,25 @@ class SessionsController < ApplicationController
     user = User.find_by(id: params[:session][:id])
     if user && user.authenticate(params[:session][:password])
       log_in user
-      redirect_to root_path, success: 'ログインに成功しました'
+      redirect_to pages_home_path, success: 'ログインに成功しました'
     else
       flash.now[:danger] = 'ログインに失敗しました'
       render :new
     end
   end
 
+  def destroy
+    log_out
+    redirect_to root_path, info: 'ログアウトしました'
+  end
+
   private
   def log_in(user)
     session[:user_id] = user.id
+  end
+
+  def log_out
+    session.delete(:user_id)
+    @current_user = nil
   end
 end
