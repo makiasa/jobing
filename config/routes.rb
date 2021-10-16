802d32c3ae49462e030c607895f05473d8c2f292
EC2 Default User <ec2-user@ip-172-31-0-13.us-east-2.compute.internal>
2021-10-16 13:46:56 +0000

    jobing

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   root 'sessions#new'
   get 'pages/home'
   get 'pages/test'
+  
+  resources :todos
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
