b377554743814c7da8b7f98b03cc9451b6ce4de5
EC2 Default User <ec2-user@ip-172-31-0-13.us-east-2.compute.internal>
2021-10-25 23:36:11 +0900

    jobing

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -1,8 +1,34 @@
 class WorksController < ApplicationController
+  def new
+    @work = Work.new
+    @workflow = Workflow.new
+  end
+  
   def show
     @departments = Department.where(firstid: current_user.department.firstid)
-    @department = Department.find_by(id: params[:department_id])
+    @department = Department.find(params[:department_id])
     @work = @department.works.find_by(firstid: params[:id])
     @workflows = @work.workflows
+  end
+  
+  def edit
+    @department = Department.find(params[:department_id])
+    @work = @department.works.find_by(firstid: params[:id])
+    @workflows = @work.workflows 
+  end
+  
+  def update
+    @department = Department.find(params[:department_id])
+    @work = @department.works.find_by(firstid: params[:id]).update(work_params)
+    @workflows = @work.workflows 
+  end
+  
+  private
+  def work_params
+    params.require(:work).permit(:work_id,:content,:deadline,:status).merge(user_id: current_user.id)
+  end
+  
+  def workflow_params
+    params.require(:workflow).permit(:work_id,:content,:deadline,:status).merge(user_id: current_user.id)
   end
 end
