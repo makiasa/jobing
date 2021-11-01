875c98994d7b0af6103b58e55f745e70f3d881fc
EC2 Default User <ec2-user@ip-172-31-0-13.us-east-2.compute.internal>
2021-11-01 11:47:12 +0900

    jobing

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -9,11 +9,11 @@ class PagesController < ApplicationController
     @user = User.find(current_user.id)
     @department = current_user.department
     @works = current_user.works.where(fiscalyear: @current_fiscalyear)
-    @current_month = Date.today.month
-    @last_month = @current_month - 1
-    @current_month_works = @works.where(period: @current_month)
-    @todos = current_user.todos
     
+    @current_month = Date.today.month
+    @current_month_works = @works.where(period: @current_month)
+    
+    @todos = current_user.todos
     @irregular_works = @works.where(period: nil)
   end
   
--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,4 +1,8 @@
 class Todo < ApplicationRecord
   belongs_to :work
   belongs_to :user
+  
+  #statusの数字を文字列に変換するためのメソッドを以下に定義する。
+  # def
+  # end
 end
