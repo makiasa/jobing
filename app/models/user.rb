f237f06e605b5eead747783893da1aa3e113a167
EC2 Default User <ec2-user@ip-172-31-0-13.us-east-2.compute.internal>
2021-12-05 11:26:24 +0900

    jobing

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   
   validates :name, presence: true
   validates :number, presence: true, uniqueness: true
+  validates :email, length: { maximum: 300 }, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }, allow_blank: true
 
   belongs_to :department, optional: true
   has_many :works
