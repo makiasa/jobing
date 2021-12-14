cbe611e94ae8e6e2f8694101f608efabe2613982
EC2 Default User <ec2-user@ip-172-31-0-13.us-east-2.compute.internal>
2021-12-14 23:54:14 +0900

    jobing

--- app/models/workflow.rb
+++ app/models/workflow.rb
@@ -6,10 +6,11 @@ class Workflow < ApplicationRecord
   
   def self.generate_csv
     CSV.generate(headers: true,encoding: Encoding::SJIS) do |csv|
-      column_names = %w(業務名 手順番号 内容 備考 ファイルパス)
+      column_names = %w(年度 業務名 手順番号 内容 備考 ファイルパス)
       csv << column_names
       all.each do |workflow|
         column_values = [
+          Constant::FISCAL_YEARS[workflow.work.fiscalyear],
           workflow.work.name,
           workflow.number,
           workflow.content,
