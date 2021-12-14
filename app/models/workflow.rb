cda31b7aff85d13c61a54494eb0c38688caead22
EC2 Default User <ec2-user@ip-172-31-0-13.us-east-2.compute.internal>
2021-12-14 23:21:32 +0900

    jobing

--- app/models/workflow.rb
+++ app/models/workflow.rb
@@ -4,15 +4,19 @@ class Workflow < ApplicationRecord
   belongs_to :work
   default_scope -> { order(:number) }
   
-  def self.csv_attributes
-    ["work_id", "number", "content", "note", "filepath"]
-  end
-  
   def self.generate_csv
-    CSV.generate(headers: true) do |csv|
-      csv << csv_attributes
+    CSV.generate(headers: true,encoding: Encoding::SJIS) do |csv|
+      column_names = %w(業務名 手順番号 内容 備考 ファイルパス)
+      csv << column_names
       all.each do |workflow|
-        csv << csv_attributes.map{|attr| workflow.send(attr)}
+        column_values = [
+          workflow.work.name,
+          workflow.number,
+          workflow.content,
+          workflow.note,
+          workflow.filepath
+                ]
+        csv << column_values
       end
     end
   end
