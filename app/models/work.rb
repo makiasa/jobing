c03dfe0ccceec3dd5708e1eb3d7592ba407bb664
EC2 Default User <ec2-user@ip-172-31-5-96.us-east-2.compute.internal>
2021-12-18 14:57:13 +0000

    workモデルのhas_many :todosにdependent: :destroyを追加

--- app/models/work.rb
+++ app/models/work.rb
@@ -5,7 +5,7 @@ class Work < ApplicationRecord
   validates :fiscalyear, presence: true
   
   belongs_to :user, optional: true
-  has_many :todos
+  has_many :todos, dependent: :destroy
   has_many :workflows, dependent: :destroy
   accepts_nested_attributes_for :workflows, reject_if: :all_blank, allow_destroy: true
   
