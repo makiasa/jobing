5093c022b61bf3d47f05c45096050256805df5a6
EC2 Default User <ec2-user@ip-172-31-5-96.us-east-2.compute.internal>
2021-12-26 03:38:22 +0000

    Admin::DepartmentsController内のupdateアクション修正:

--- app/controllers/admin/departments_controller.rb
+++ app/controllers/admin/departments_controller.rb
@@ -25,6 +25,7 @@ class Admin::DepartmentsController < ApplicationController
   def confirm_new
     @department = Department.new(department_params)
     @departments = Department.where(ancestry: nil).or(Department.where("ancestry not like?", "%/%")).order(:number)
+    @reference_departments = Department.all.order(:number)
     if params[:department][:ancestry].present?
       @parent_department = Department.find(params[:department][:ancestry]) 
     else
@@ -32,21 +33,16 @@ class Admin::DepartmentsController < ApplicationController
     end
     render :new unless @department.valid?
   end
-
-  def edit
-    @department = Department.find(params[:id])
-    @parent_department = @department.parent
-    @departments = Department.where(ancestry: nil).or(Department.where("ancestry not like?", "%/%")).order(:number)
-    
-    @reference_departments = Department.all.order(:number)
-  end
   
   def create
     @department = Department.new(department_params)
-    @departments = Department.where(ancestry: nil).or(Department.where("ancestry not like?", "%/%")).order(:number)
     @department.ancestry = nil  if params[:department][:ancestry].blank?
     
-    render(:new) and return if params[:back].present?
+    if params[:back].present? #確認画面の「戻る」ボタンがトリガー
+      @departments = Department.where(ancestry: nil).or(Department.where("ancestry not like?", "%/%")).order(:number)
+      @reference_departments = Department.all.order(:number)
+      render(:new) and return 
+    end
     
     if @department.save
       if params[:department][:ancestry].present? && Department.find(params[:department][:ancestry]).root?
@@ -56,14 +52,21 @@ class Admin::DepartmentsController < ApplicationController
       end
       redirect_to admin_department_path(@department), notice: "部署「#{@department.name}」を登録しました"
     else
+      @departments = Department.where(ancestry: nil).or(Department.where("ancestry not like?", "%/%")).order(:number)
       render :new
     end
   end
   
-  def update #ここをcreateと同じ論理で修正
+  def edit
     @department = Department.find(params[:id])
+    @parent_department = @department.parent
     @departments = Department.where(ancestry: nil).or(Department.where("ancestry not like?", "%/%")).order(:number)
     
+    @reference_departments = Department.all.order(:number)
+  end
+  
+  def update
+    @department = Department.find(params[:id])
     if @department.update(department_params)
       if params[:department][:ancestry].present? && Department.find(params[:department][:ancestry]).root?
         @department.update(ancestry: "#{params[:department][:ancestry]}")
@@ -72,11 +75,18 @@ class Admin::DepartmentsController < ApplicationController
       end
       redirect_to admin_department_path(@department), notice: "部署「#{@department.name}」を更新しました"
     else
-      if params[:department][:ancestry].blank?
+      if params[:department][:ancestry].blank? #1.update失敗の理由が「params[:department][:ancestry]が""（空の文字列）」の場合を想定
         @department.ancestry = nil
-        @department.update(department_params_except_column_ancestry)
-        redirect_to admin_department_path(@department), notice: "部署「#{@department.name}」を更新しました"
-      else
+        if @department.save
+          redirect_to admin_department_path(@department), notice: "部署「#{@department.name}」を更新しました"
+        else
+          @departments = Department.where(ancestry: nil).or(Department.where("ancestry not like?", "%/%")).order(:number)
+          @reference_departments = Department.all.order(:number)
+          render :edit
+        end
+      else #1.update失敗の理由がその他（バリデーションエラー等）の場合
+        @departments = Department.where(ancestry: nil).or(Department.where("ancestry not like?", "%/%")).order(:number)
+        @reference_departments = Department.all.order(:number)
         render :edit
       end
     end
